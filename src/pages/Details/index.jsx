36f463259d6b622a58b075efd8ee87f398e90c39
Matheus <matheus.silva246@fatec.sp.gov.br>
2024-01-31 21:36:46 -0300

    folder Details and css

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,8 +1,11 @@
+import { Container } from './style';
+
 export function Details(){
+
   return(
-    <>
+    <Container>
       <h1>Hello word!</h1>
       <span>Matheus Toledo</span>
-    </>
+    </Container>
   )
 }
