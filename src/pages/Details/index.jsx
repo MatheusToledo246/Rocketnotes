09d078e29c45c3ec13c489037478f734f2f2382e
Matheus <matheus.silva246@fatec.sp.gov.br>
2024-02-02 22:46:01 -0300

    add button

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,17 @@
 import { Container } from './style';
 
+import { Button } from '../../components/Button';
+
 export function Details(){
 
   return(
     <Container>
       <h1>Hello word!</h1>
       <span>Matheus Toledo</span>
+
+      <Button title="Entrar" />
+      <Button title="Cadastrar" />
+      <Button title="Voltar" />
     </Container>
-  )
+  );
 }
