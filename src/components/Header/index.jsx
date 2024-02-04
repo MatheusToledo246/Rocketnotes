f20dc8ddea8f750250af7f755fa7a1be273fcaa2
Matheus <matheus.silva246@fatec.sp.gov.br>
2024-02-04 13:30:52 -0300

    header style

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,18 @@
-import { Container } from "./style";
+import { Container, Profile } from "./style";
 
 export function Header(){
     return (
         <Container>
+            <Profile>
+                <img src="https://github.com/MatheusToledo246.png"
+                alt="Foto do usuário"
+                />
+
+                <div>
+                    <span>Bem-vindo</span>
+                    <strong>Matheus Toledo</strong>
+                </div>
+            </Profile>
 
         </Container>
     );
