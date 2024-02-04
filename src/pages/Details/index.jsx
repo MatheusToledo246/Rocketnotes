73c807448d30cbf67be54cda509be325d19f41d7
Matheus <matheus.silva246@fatec.sp.gov.br>
2024-02-04 14:40:36 -0300

    first section

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,13 +1,23 @@
-import { Container } from './style';
+import { Container, Links } from './style';
 
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
+import { Section } from '../../components/Section';
+
 
 export function Details(){
 
   return(
     <Container>
       <Header />
+
+      <Section title="Links úteis">
+        <Links>
+          <li><a href="#">https://www.rocketseat.com.br/</a></li>
+          <li><a href="#">https://www.rocketseat.com.br/</a></li>
+
+        </Links>
+      </Section>
 
       <Button title="Voltar" />
     </Container>
