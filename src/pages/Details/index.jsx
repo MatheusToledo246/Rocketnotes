25c7033bd11706d27252cec5f7e1c5a272233ebd
Matheus <matheus.silva246@fatec.sp.gov.br>
2024-02-04 18:06:36 -0300

    finalizing details

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from './style';
+import { Container, Links, Content } from './style';
 
 import { Tag } from '../../components/Tag';
 import { Button } from '../../components/Button';
@@ -15,22 +15,36 @@ export function Details(){
     <Container>
       <Header />
 
-      <ButtonText title="Excluir nota" />
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
 
-      <Section title="Links úteis">
-        <Links>
-          <li><a href="#">https://www.rocketseat.com.br/</a></li>
-          <li><a href="#">https://www.rocketseat.com.br/</a></li>
+          <h1>
+            Introdução ao React
+          </h1>
 
-        </Links>
-      </Section>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing 
+            elit. Perspiciatis non nostrum nobis amet velit, 
+            corporis sequi eligendi, earum debitis cumque ducimus, 
+            neque vitae recusandae. Facere labore repudiandae ad 
+            illum magni?
+          </p>
+          <Section title="Links úteis">
+          <Links>
+            <li><a href="#">https://www.rocketseat.com.br/</a></li>
+            <li><a href="#">https://www.rocketseat.com.br/</a></li>
+          </Links>
+          </Section>
 
-      <Section title="Marcadores">
-        <Tag title="express" />
-        <Tag title="node.js" />
-      </Section>
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="node.js" />
+          </Section>
 
-      <Button title="Voltar" />
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   );
 }
