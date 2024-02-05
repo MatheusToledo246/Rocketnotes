722c1cb32631f62a310c53baf0823b65559b35ae
Matheus <matheus.silva246@fatec.sp.gov.br>
2024-02-04 21:16:34 -0300

    create notes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,10 @@
 import { FiPlus, FiSearch } from 'react-icons/fi';
 import { Container, Brand, Menu, Search, Content, NewNote } from './style'
 
-import { Header } from '../../components/Header';
+import { Note } from '../../components/Note';
 import { Input } from '../../components/Input';
+import { Header } from '../../components/Header';
+import { Section } from '../../components/Section';
 import { ButtonText } from '../../components/ButtonText';
 
 
@@ -23,11 +25,20 @@ export function Home(){
             </Menu>
 
             <Search>
-                <Input  placeholder="Pesquisar pelo titulo" icon={FiSearch} />
+                <Input  placeholder="Pesquisar pelo titulo"  />
             </Search>
-
+        
             <Content>
-
+                <Section title="Minhas notas">
+                    <Note data={{ 
+                        title: 'React',
+                         tags: [
+                            {id: '1', name: 'react'},
+                            {id: '2', name: 'rocketseat'}
+                        ]
+                         }} 
+                         />
+                </Section>
             </Content>
 
             <NewNote>
